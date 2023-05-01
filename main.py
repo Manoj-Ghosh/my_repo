0fee9cc37d8b42a4e663e5f69b4d5d7018e67cfb
Manoj-Ghosh <35732653+Manoj-Ghosh@users.noreply.github.com>
2023-05-01 17:59:55 +0530

    try_to_upload_testing

---
+++ main.py
@@ -0,0 +1,22 @@
+'''from numpy import random
+
+x=random.randint(10, size=(3,3))
+print(x,"\n\n\n")
+
+
+y=random.randint(5, size=(3,3))
+print(y,"\n\n\n")
+
+p = 
+
+m = x * y
+
+print(m)'''
+
+
+import array as arr 
+
+numbers = arr.array('i',[10,20,30])
+
+#search for the index of the value 10
+print(numbers.index(30))
